561f6df4aa0d966ed71cf4c59c767913682a7e72
Nelly Barret <nelly.barret@inria.fr>
2025-06-05 17:34:09 +0200

    declare VCF files on /home/data

--- src/constants/structure.py
+++ src/constants/structure.py
@@ -8,6 +8,7 @@ DOCKER_FOLDER_DATA = os.path.join(DOCKER_FOLDER, "real-data")
 DOCKER_FOLDER_METADATA = os.path.join(DOCKER_FOLDER, "metadata")
 # DOCKER_FOLDER_ANONYMIZED_PATIENT_IDS = os.path.join(DOCKER_FOLDER, "pids")
 DOCKER_FOLDER_TEST = os.path.join(DOCKER_FOLDER, "datasets", "test")
+VCF_MOUNTED_DOCKER = "/home/data"
 DB_CONNECTION = "mongodb://mongo:27017/"
 
 TEST_DB_NAME = "better_test"
